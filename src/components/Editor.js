1d30fee317515dbf70118a2fd43783879a40db7c
Nguyễn Công Cương <cuongnc.min@gmail.com>
2021-08-31 23:42:54 +0700

    feat: material-ui

--- src/components/Editor.js
+++ src/components/Editor.js
@@ -1,17 +1,18 @@
 import React from "react"
+import { TextareaAutosize } from '@material-ui/core';
 
 function Editor(props) {
     return (
-        <section className="markdown__editor relative bg-white p-8">
-            <form className="">
-                <textarea
-                    id="editor"
-                    className="h-screen w-full p-8"
-                    value={props.value}
-                    onChange={props.handleChange}
-                />
-            </form>
-        </section>
+      <section className="markdown__editor relative bg-white p-8">
+				<TextareaAutosize
+					aria-label="empty textarea"
+					placeholder="Empty"
+					id="editor"
+					value={props.value}
+					onChange={props.handleChange}
+					className="w-full font-mono"
+				/>
+      </section>
     )
 }
 
